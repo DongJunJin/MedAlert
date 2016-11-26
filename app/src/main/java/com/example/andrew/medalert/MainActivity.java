8b89063243fc4a7d5b5525971dc6571e365b0185
Andrew-Jin <D29Jin@uwaterloo.ca>
2016-11-26 15:15:57 -0500

    hue

---
+++ app/src/main/java/com/example/andrew/medalert/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.andrew.medalert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button stages = (Button) findViewById(R.id.stages);
+        Button pReg = (Button) findViewById(R.id.pReg);
+        Button symptoms = (Button) findViewById(R.id.symptoms);
+        Button messages = (Button) findViewById(R.id.messages);
+
+        stages.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
+
+        pReg.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
+
+        symptoms.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
+
+        messages.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/andrew/medalert/PatientRegistration.java
@@ -0,0 +1,11 @@
+package com.example.andrew.medalert;
+
+import android.app.Activity;
+
+/**
+ * Created by andrew on 11/26/16.
+ */
+
+public class PatientRegistration extends Activity {
+    
+}
