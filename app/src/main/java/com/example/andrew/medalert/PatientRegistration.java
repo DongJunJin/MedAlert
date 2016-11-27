36ba00fb911a8fdc450ac070e95b22e5a5e67bc6
Andrew-Jin <D29Jin@uwaterloo.ca>
2016-11-27 05:12:40 -0500

    added activity

--- app/src/main/java/com/example/andrew/medalert/PatientRegistration.java
+++ app/src/main/java/com/example/andrew/medalert/PatientRegistration.java
@@ -1,17 +1,45 @@
 package com.example.andrew.medalert;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by andrew on 11/26/16.
  */
 
 public class PatientRegistration extends AppCompatActivity {
+
+    String name;
+    String poNum;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.patient_registration);
+
+        final EditText nameText = (EditText) findViewById(R.id.namefield);
+        final EditText poNumb = (EditText) findViewById(R.id.poNum);
+
+        Button sendAlert = (Button) findViewById(R.id.send_alert);
+
+        sendAlert.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(nameText == null || poNumb == null){
+                    Toast.makeText(PatientRegistration.this, "Fields are Empty!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    name = nameText.getText().toString();
+                    poNum = poNumb.getText().toString();
+                    Toast.makeText(PatientRegistration.this, "Entry Added!", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        });
+
     }
 }
