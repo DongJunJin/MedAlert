b47fe4be9edf55008dac2adb8e7475aab64f801a
Andrew-Jin <D29Jin@uwaterloo.ca>
2016-11-26 23:50:32 -0500

    added alerts page

---
+++ app/src/main/java/com/example/andrew/medalert/Alerts.java
@@ -0,0 +1,16 @@
+package com.example.andrew.medalert;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by andrew on 11/26/16.
+ */
+
+public class Alerts extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alerts);
+    }
+}
