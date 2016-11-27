c90cb64b6504b3b00a5d8a253bc1ff2babf85046
Andrew-Jin <D29Jin@uwaterloo.ca>
2016-11-27 07:34:11 -0500

    sms functionality on patientreg

--- app/src/main/java/com/example/andrew/medalert/PatientRegistration.java
+++ app/src/main/java/com/example/andrew/medalert/PatientRegistration.java
@@ -1,7 +1,12 @@
 package com.example.andrew.medalert;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,13 +18,36 @@ import android.widget.Toast;
 
 public class PatientRegistration extends AppCompatActivity {
 
-    String name;
-    String poNum;
-
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.patient_registration);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SEND_SMS)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        1);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
 
         final EditText nameText = (EditText) findViewById(R.id.namefield);
         final EditText poNumb = (EditText) findViewById(R.id.poNum);
@@ -29,13 +57,20 @@ public class PatientRegistration extends AppCompatActivity {
         sendAlert.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                if(nameText == null || poNumb == null){
+
+                String name = nameText.getText().toString();
+                String poNum = poNumb.getText().toString();
+
+                if(name.matches("") || poNum.matches("")){
                     Toast.makeText(PatientRegistration.this, "Fields are Empty!", Toast.LENGTH_SHORT).show();
+                    return;
                 }
                 else{
-                    name = nameText.getText().toString();
-                    poNum = poNumb.getText().toString();
-                    Toast.makeText(PatientRegistration.this, "Entry Added!", Toast.LENGTH_SHORT).show();
+
+                    String finale = name + ", +1" + poNum;
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage("+16472819071", null, finale, null, null);
+                    Toast.makeText(PatientRegistration.this, "Message Sent!", Toast.LENGTH_LONG).show();
                     finish();
                 }
             }
